39b085c5050ee84800a5ad7984efe49bf3200870
Percy <percival.ngan@gmail.com>
2020-01-07 14:17:15 -0600

    Added TruckController

---
+++ FoodTruckTracker/FoodTruckTracker/Model Controllers/ConsumerController.swift
@@ -0,0 +1,29 @@
+//
+//  ConsumerController.swift
+//  FoodTruckTracker
+//
+//  Created by Percy Ngan on 1/7/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+class ConsumerController {
+
+	var user: Any?
+	var token: String?
+
+	static let shared = ConsumerController()
+
+	func register() {
+
+	}
+
+	func logIn() {
+
+	}
+
+	func saveToPersistentStore() {
+
+	}
+}
